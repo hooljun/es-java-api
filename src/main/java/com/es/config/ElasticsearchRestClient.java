c4b9b5ecb282278f395700223df5cbebb76712bc
luohaipeng <164132877@qq.com>
2019-01-23 18:00:38 +0800

    修改

--- src/main/java/com/es/config/ElasticsearchRestClient.java
+++ src/main/java/com/es/config/ElasticsearchRestClient.java
@@ -8,6 +8,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 @Configuration
 public class ElasticsearchRestClient {
 
@@ -21,12 +24,13 @@ public class ElasticsearchRestClient {
     //@Bean(autowire = Autowire.BY_NAME, name = "restHighLevelClient")
     @Bean
     public RestHighLevelClient client() {
-        HttpHost[] httpHosts = new HttpHost[ips.length];
-        for(int i=0;i<ips.length;i++){
-            httpHosts[i] = HttpHost.create(ips[i]);
-        }
+        HttpHost[] httpHosts = (HttpHost[]) Stream.of(ips).map(this::createHttpHost).collect(Collectors.toList()).toArray();
         RestClientBuilder builder = RestClient.builder(httpHosts);
         return new RestHighLevelClient(builder);
+    }
+
+    private HttpHost createHttpHost(String ip) {
+        return HttpHost.create(ip);
     }
 
 }
